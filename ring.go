33c43a8b7d2b29ce310f06b11025fbe6dd45a869
Alexander Grafov <grafov@gmail.com>
2017-08-22 10:51:34 +0300

    Expiriment with strings

--- ring.go
+++ ring.go
@@ -3,8 +3,6 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"runtime/trace"
 	"sync/atomic"
 	"time"
 )
@@ -16,7 +14,7 @@ type ringbuf struct {
 	RCount [buflen]int64
 }
 
-var maxDataItems = 10
+var maxDataItems = 100
 
 func consume(buf *ringbuf, name int) {
 	var (
@@ -27,24 +25,26 @@ func consume(buf *ringbuf, name int) {
 	checkCounter:
 		rcount := atomic.LoadInt64(&buf.RCount[cur])
 		// Check when generator writing to this row (-1) or the row
-		// has been read by all consumers.
+		// is empty (0).
 		if rcount <= 0 {
 			time.Sleep(10 * time.Microsecond)
 			w++
-			fmt.Println(name, cur, "wait for data", w)
+			//			fmt.Println(name, cur, "wait for data", w)
 			goto checkCounter
 		}
+
+		// RCount should still be > 0 in this place.
 
 		// Generator will wait until RCount=0 so it can be safely read
 		// from the row here.
 		val := buf.Rows[cur]
 
-		// The row has been consumed so decrease read count.
-		atomic.AddInt64(&buf.RCount[cur], -1)
-
 		// Pass data to something
 		// ...
-		fmt.Println(name, cur, val.(int))
+		fmt.Println(name, cur, val.(string))
+
+		// The row has been consumed so decrease read count.
+		atomic.AddInt64(&buf.RCount[cur], -1)
 
 		// Scroll to the next row.
 		cur++
@@ -52,72 +52,75 @@ func consume(buf *ringbuf, name int) {
 	}
 }
 
-var workers = 4
+var (
+	generators = 2
+	consumers  = 3
+)
 
-func generate(data *ringbuf) {
-	for i := 0; i < maxDataItems; i++ {
-		go put(data, i)
-	}
-}
-
-func put(rb *ringbuf, value interface{}) {
+func generate(rb *ringbuf, name int) {
 	var (
 		cur int64
 		adr = &rb.RCount[cur]
 	)
-waitForWorkers:
-	// If data has been read by all workers then the RCount must
-	// be zero.  For clarity generator set it to -1. It will
-	// prevent this row from reading by the workers.
-	if !atomic.CompareAndSwapInt64(adr, 0, -1) {
-		if rc := atomic.LoadInt64(adr); rc == -1 {
-			// The row already has been taken by another writer.
-			// Scroll to the next one.
-			cur++
-			cur = cur % buflen
-			adr = &rb.RCount[cur]
+	for i := 0; i < maxDataItems; i++ {
+	waitForWorkers:
+		// If data has been read by all workers then the RCount must
+		// be zero. For clarity generator set it to -1. It will
+		// prevent this row from reading by the workers.
+		if !atomic.CompareAndSwapInt64(adr, 0, -1) {
+			if rc := atomic.LoadInt64(adr); rc == -1 {
+				// The row already has been taken by another writer.
+				// Scroll to the next one.
+				cur++
+				cur++
+				cur = cur % buflen
+				adr = &rb.RCount[cur]
+			} else {
+				// The row hasn't read yet. So the generator waits for all
+				// the consumers read it.
+				time.Sleep(50 * time.Nanosecond)
+			}
 			goto waitForWorkers
 		}
-		// The row hasn't read yet. So the generator waits for all
-		// the consumers read it.
-		time.Sleep(100 * time.Nanosecond)
-		goto waitForWorkers
+
+		// vvv CRITICAL CODE SECTION: WRITE TO THE ROW IS ALLOWED HERE
+
+		// The row protected by Rcount=-1 here.
+		// So it can safely assign a new value here.
+		rb.Rows[cur] = fmt.Sprintf("g%d-%d", name, i)
+
+		fmt.Println(fmt.Sprintf("G%d", name), cur, rb.Rows, rb.RCount)
+
+		// Reinitialize RCount for enable reading again: set it to the number of workers.
+		atomic.StoreInt64(adr, int64(consumers))
+
+		// ^^^ THE END OF CRITICAL CODE SECTION
+
+		// Scroll to the next row.
+		cur++
+		cur = cur % buflen
 	}
-
-	// vvv CRITICAL CODE SECTION: WRITE TO THE ROW IS ALLOWED HERE
-
-	// The row protected by Rcount=-1 here.
-	// So it can safely assign a new value here.
-	rb.Rows[cur] = value
-
-	// Reinitialize RCount for enable reading again: set it to the number of workers.
-	atomic.StoreInt64(&rb.RCount[cur], int64(workers))
-
-	// ^^^ THE END OF CRITICAL CODE SECTION
-
-	// Here the data from the row could be read by any worker.
-	fmt.Println("G", cur, rb.Rows)
-
-	// Scroll to the next row.
-	cur++
-	cur = cur % buflen
 }
 
 func main() {
-	tfile, _ := os.OpenFile("/tmp/trace.out", os.O_CREATE|os.O_RDWR, 0666)
-	trace.Start(tfile)
+	//	tfile, _ := os.OpenFile("/tmp/trace.out", os.O_CREATE|os.O_RDWR, 0666)
+	//	trace.Start(tfile)
 
 	var rb ringbuf
+
+	// Start consumers. The generator generates sequence, stores it
+	// to ringbuf and exits.
+	for i := 0; i < generators; i++ {
+		go generate(&rb, i)
+	}
+
 	// Create and run workers.
-	for i := 0; i < workers; i++ {
+	for i := 0; i < consumers; i++ {
 		go consume(&rb, i)
 	}
 
-	// Start generator. It generates sequence, stores it to ringbuf and exits.
-	generate(&rb)
+	time.Sleep(500 * time.Millisecond)
+	//	fmt.Printf("%#v\n", rb.Rows)
 
-	time.Sleep(1 * time.Second)
-	fmt.Printf("%#v\n", rb.Rows)
-
-	trace.Stop()
+	//	trace.Stop()
 }
